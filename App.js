2b928d4d34dea36e4b9e6ad87fbd01ed1e5e52e1
hamzasaleem2397 <64617903+hamzasaleem2397@users.noreply.github.com>
2022-04-06 03:45:25 +0500

    app icon and splash screen

--- App.js
+++ App.js
@@ -1,37 +1,32 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import MyStack from './src/comonents/Stack/Stack';
 import LoginStack from './src/comonents/Stack/LoginStack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import SplashScreen from 'react-native-splash-screen';
 export default function App() {
   const [save, setSave] = React.useState({});
 
-
-
   const get_data = async () => {
-
     try {
       const value = await AsyncStorage.getItem('uid');
       if (value !== 'null') {
         // We have data!!
-        setSave(JSON.parse(value))
+        setSave(JSON.parse(value));
       }
     } catch (error) {
       // Error retrieving data
     }
-  }
+  };
   React.useEffect(() => {
-    get_data()
-  }, [get_data, save])
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+  }, []);
 
   return (
     <NavigationContainer>
-      {save?.CONFIRM_PASSWORD ?
-        <MyStack />
-        :
-        <LoginStack />
-      }
+      {save?.CONFIRM_PASSWORD ? <MyStack /> : <LoginStack />}
     </NavigationContainer>
   );
 }
